065e307896b9ab81defdf7f03fee5dc36691e27c
MoeweX <Jonathan@Hasenburg.de>
2019-01-14 10:07:25 +0100

    Add plain project

---
+++ plain/src/main/java/de/hasenburg/plain/Brain.java
@@ -0,0 +1,14 @@
+package de.hasenburg.plain;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Brain {
+
+	private static final Logger logger = LogManager.getLogger();
+
+	public static void main (String[] args) {
+		logger.info("Hello World!");
+	}
+
+}
